db0b2811f56e6aba99e6eac18978fa28729e2776
MuhammadUmar09 <mughal99kings@gmail.com>
2024-05-01 10:49:00 +0500

    myside

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function for carousel
 const carouselImages = document.querySelector('.carousel-images');
 const images = document.querySelectorAll('.carousel-images img');
 
@@ -14,3 +15,20 @@ function slide() {
 }
 
 setInterval(slide, 3000); // Change image every 3 seconds
+
+// Function for image animation based on scroll events
+const imageContainer = document.getElementById('imageContainer');
+const images1 = document.querySelectorAll('.image-wrapper1');
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    const windowWidth = window.innerWidth;
+
+    images1.forEach((image, index) => {
+        const offset = index * windowWidth / 2;
+        const distanceFromTop = image.offsetTop - scrollY;
+        const translateX = (windowWidth / 2) - offset - distanceFromTop * 0.5;
+
+        image.style.transform = `translateX(${translateX}px)`;
+    });
+});
